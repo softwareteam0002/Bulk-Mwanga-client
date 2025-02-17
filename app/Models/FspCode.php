4f47c33522bab418790463ad729e733aba5ecd48
costantine-tarimo <112546624+costantine-tarimo@users.noreply.github.com>
2025-02-17 17:00:29 +0300

    Add FspCode and HdpayToken models
    Introduce two new Eloquent models: FspCode and HdpayToken. These models correspond to the 'fsp_codes' and 'hdpay_token' tables, respectively, with guarded attributes set to an empty array for mass assignment handling.

---
+++ app/Models/FspCode.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FspCode extends Model
+{
+    protected $table = 'fsp_codes';
+
+    protected $guarded = [];
+}
---
+++ app/Models/HdpayToken.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HdpayToken extends Model
+
+{
+    protected $table = 'hdpay_token';
+
+    protected $guarded = [];
+
+}
